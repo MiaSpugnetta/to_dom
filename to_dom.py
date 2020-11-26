1ecfb19ef1ae0031dd8587d1a177e8a87ce0bfa7
MiaSpugnetta <martina.figlia@gmail.com>
2020-11-26 10:16:47 +0100

    finished cleanup, updated requirements.txt, removed test files

--- to_dom.py
+++ to_dom.py
@@ -98,18 +98,6 @@ def parse_dict(msg_dict):
         # subject2: [list of dictionaries where every dictionary has keys id, text]
         #}
 
-
-    # {TODO: find a way to replace "ressource" with "resource"
-        #new_dict = defaultdict()
-        ##replace = "Ressource"
-        ##replace_with = "Resource"
-        #new_dict = {i:j for i,j in return_dict.items() if i != "Ressource"}
-        #new_dict["Resource"] = return_dict["Ressource"]
-
-        #return_dict['Resource'] = return_dict.pop('Ressource')
-        #return_dict['subject'] = "Resource"
-        #}
-
     return return_dict  # Returns a dictionary of dictionaries where the key is the subject (category) of the email and the value is a list of dictionaries that have that category as subject. Each of these subdictionary has then id and text as keys.
 
 
@@ -121,7 +109,7 @@ dict_of_msgs = msg_dict.copy()  # Create copy of original dictionary
 for id,msg in dict_of_msgs.items():  # For key, value (where key=id and vale={msg}) in dictionary of messages:
     for k,field in msg.items():  # For key, value (where key='subject', 'text and values are the subject and text of the msg) in msg subdictionary:
         if k in ['subject']:  # If key == the str 'subject' (or if the k is contained (is equal to on of the entries) in the list that contains the str 'subject':
-            msg[k]=msg[k].capitalize()  # Capitalise the value of the key 'subject'.
+            msg[k] = msg[k].capitalize()  # Capitalise the value of the key 'subject'.
         # (Can also be represented as:
         #  For releant_entry in relevant_list: # relevant_list = ['this', 'that', 'those']
         #    If k == releant_entry:
