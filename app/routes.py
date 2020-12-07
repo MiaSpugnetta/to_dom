28208adf4d25e4e9720cf291414ad7903afd919e
MiaSpugnetta <martina.figlia@gmail.com>
2020-12-07 17:44:50 +0100

    updated routes documentation, cleaned test_2

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app, server_name
-from flask import redirect, render_template, url_for
+from flask import redirect, render_template
 from abstractions.database import get_db_entries, get_entry, update_entry
 from collections import defaultdict
 from to_dom import get_dict_of_msg
@@ -10,7 +10,7 @@ from .forms import ButtonInput
 @app.route('/index', methods=['GET', 'POST'])
 def index():
 
-    all_entries = get_db_entries()
+    all_entries = get_db_entries()  # Get all entries from database
     button = ButtonInput()
 
     if button.validate_on_submit():
@@ -37,10 +37,6 @@ def test():
         #              <subject2>: [{ }, {}, ...},
         #               ... }
 
-    #list_of_subjects = []
-    #for subject in msg_dict:
-    #   list_of_subjects.append(subject)
-
     entry_list = []
 
     for sub, msg_list in msg_dict.items():
@@ -54,23 +50,8 @@ def test():
         #                                         {...}
         #                                         ]
         #                                },
-        #                                { 'subject':<subject2>, 'mails':[{}, {}, {}]}, ... ]
+        #                                { 'subject':<subject2>, 'mails':[{}, {}, {}] }, ... ]
 
-
-    #for sub in list_of_subjects:
-    #    mail_list = get_db_entries(sub)  # Fetch entries from db with specified subject
-    #    dict_by_sub = {"subject":sub, "mails":mail_list}  # Create dict with key:sub and key:[list of all {entries} that have that subject]
-    #    entry_list.append(dict_by_sub)  # Append created dict to the list
-    #    # Create a list structured as: [
-    #    #                               {'subject':<subject>,
-    #    #                                'mails':[
-    #    #                                         {'key':<key>, 'subject':<subject>, 'text':<text>},
-    #    #                                         {...},
-    #    #                                         {...}
-    #    #                                         ]
-    #    #                                },
-    #    #                                { 'subject':<subject2>, 'mails':[{}, {}, {}]}, ... ]
-#
     return render_template("test.html", list_of_entries=entry_list)
 
 
@@ -90,7 +71,7 @@ def test_2():
         #              <subject2>: [{ }, {}, ...},
         #               ... }
 
-        if entry['done'] == False:
+        if entry['done'] == False:  # Count the number of entries not marked as done
             number_of_msg += 1
 
     entry_list = []
@@ -113,52 +94,48 @@ def test_2():
     return render_template("test_2.html", list_of_entries=entry_list, number_of_msg=number_of_msg)
 
 
+# Define how to mark  an entry in db as 'done'.
 @app.route('/mark_as_done/<key>', methods=['GET', 'POST'])
-def mark_as_done(key):
+def mark_as_done(key):  # Key to insure that it's the right entry
 
-    entry = get_entry(key)
-
+    entry = get_entry(key)  # Fetch the entry from db
     assert entry['key'] == key, 'wrong item'
-    update = {'done': True}
-    update_entry(update, key)
 
-    #return redirect(url_for('test_2'))
-    return redirect(f'{server_name}/test_2')
-    #return redirect(f'{server_name}/test_2')#("https://yi0xmp.deta.dev/test_2", code=302)#f'Entry {key} removed!'#redirect(app.config['SERVER_NAME']+ '/test_2')
+    update = {'done': True}  # Assign to 'done' field value=True
+    update_entry(update, key)  # Update the entry in db
+
+    return redirect(f'{server_name}/test_2')  # Redirect to same page this action can be performed
 
 
 @app.route('/done', methods=['GET', 'POST'])
 def done():
-    list_of_entries = get_db_entries(done=True)
-    number_of_items = (len(list_of_entries))
+    list_of_entries = get_db_entries(done=True)  # Fetch from db only the entries marked as 'done'
+    number_of_items = (len(list_of_entries))  # Count the number of entries marked as 'done'
+
     msg_dict = defaultdict(list)
 
-    # Returns a dictionary of dictionaries from the list_of_entries.
+    # Create a dictionary of dictionaries from the list_of_entries.
     for entry in list_of_entries:
         subject = entry.pop('subject')  # Pops the key from the inner dict, returns the value
         msg_dict[subject].append(entry)  # The entry becomes the value of the
 
     entry_list = []
-
     for sub, msg_list in msg_dict.items():
 
-        dict_by_sub = {'subject':sub, 'mails':msg_list}
-        entry_list.append(dict_by_sub)
+        dict_by_sub = {'subject':sub, 'mails':msg_list}  # Create dict with key:sub and key:[list of all {entries} that have that subject]
+        entry_list.append(dict_by_sub)  # Append created dict to the list
 
-    return render_template("done.html", entry_list=entry_list, number_of_items=number_of_items, server_name=server_name)
+    return render_template("done.html", entry_list=entry_list, number_of_items=number_of_items)
 
 
+# Define how to mark  an entry in db as 'undone'.
 @app.route('/mark_undone/<key>', methods=['GET', 'POST'])
 def mark_undone(key):
 
-    entry = get_entry(key)
-    assert entry['key'] == key, 'wrong item'
+    entry = get_entry(key)  # Fetch the entry from db
+    assert entry['key'] == key, 'wrong item'  # Make sure is the right entry
 
-    update = {'done': False}
-    update_entry(update, key)
+    update = {'done': False}  # Update 'done' field
+    update_entry(update, key)   # Update entry in db
 
-    #return redirect(url_for('done'))  # This if running locally
-    return redirect(f'{server_name}/done')#, code=302)
-
-    #return redirect(f'{server_name}/done')#("https://yi0xmp.deta.dev/done", code=302)#f'Entry {key} restored!'
-    #return redirect(url_for('done', _external=True))
+    return redirect(f'{server_name}/done')  # Redirect to same page this action can be performed
