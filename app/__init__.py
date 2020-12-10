e098d5cea95d52e48811de9a64334ea6d72fbc81
MiaSpugnetta <martina.figlia@gmail.com>
2020-12-10 12:57:21 +0100

    updated app documentation, removed test, moved original index page with button to test page, original test_2 is now index

--- app/__init__.py
+++ app/__init__.py
@@ -7,13 +7,7 @@ app = Flask(__name__)  # The `__name__` variable passed to the Flask class is a 
 #####################################
 # This section has been added after introducing wtf flask form. A secret key generated tokes is necessary to validate the forms.
 
-#config = get_config('../config.json')
-#secret_key = config['secret_key']
-#app.config(secret_key)
-
 app.config.from_object(Config)  # Import SECRET_KEY from Config class in config file.
-#app.config['SERVER_NAME'] = server_name
-#server_name = app.config["SERVER_NAME"]
 
 ######################################
 # Import routes module
--- app/routes.py
+++ app/routes.py
@@ -9,54 +9,6 @@ from .forms import ButtonInput
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
-
-    all_entries = get_db_entries()  # Get all entries from database
-    button = ButtonInput()
-
-    if button.validate_on_submit():
-
-        get_dict_of_msg()
-
-        print('Updated!')
-        return redirect('index')#f"""You are the favourite and your database has been updated, refresh the page!"""
-
-    return render_template("index.html", all_entries=all_entries, form=button)
-
-
-@app.route('/test', methods=['GET', 'POST'])
-def test():
-    list_of_entries = get_db_entries()  # Return a list of all the entries in db as dictionaries in the form of [{subject:<sub>, key:<key>, text:<text>}, {}, ... ].
-
-    msg_dict = defaultdict(list)
-
-    # Returns a dictionary of dictionaries from the list_of_entries.
-    for entry in list_of_entries:
-        subject = entry.pop('subject')  # Pops the key from the inner dict, returns the value
-        msg_dict[subject].append(entry)  # The entry becomes the value of the key=subject.
-        # msg_dict = { <subject1> : [{'key':<key>, 'text':<text>}, {}, ...],
-        #              <subject2>: [{ }, {}, ...},
-        #               ... }
-
-    entry_list = []
-
-    for sub, msg_list in msg_dict.items():
-        dict_by_sub = {'subject': sub, 'mails': msg_list}  # Create dict with key:sub and key:[list of all {entries} that have that subject]
-        entry_list.append(dict_by_sub)  # Append created dict to the list
-        # Create a list structured as: [
-        #                               {'subject':<subject>,
-        #                                'mails':[
-        #                                         {'key':<key>, 'subject':<subject>, 'text':<text>},
-        #                                         {...},
-        #                                         {...}
-        #                                         ]
-        #                                },
-        #                                { 'subject':<subject2>, 'mails':[{}, {}, {}] }, ... ]
-
-    return render_template("test.html", list_of_entries=entry_list)
-
-
-@app.route('/test_2', methods=['GET', 'POST'])
-def test_2():
     # TODO: get_db_entries doesn't work properly
     list_of_entries = get_db_entries()  # Return a list of all the entries in db as dictionaries in the form of [{subject:<sub>, key:<key>, text:<text>}, {}, ... ].
 
@@ -67,8 +19,9 @@ def test_2():
     for entry in list_of_entries:
         subject = entry.pop('subject')  # Pops the key from the inner dict, returns the value
         msg_dict[subject].append(entry)  # The entry becomes the value of the key=subject.
-        # msg_dict = { <subject1> : [{'key':<key>, 'text':<text>}, {}, ...],
-        #              <subject2>: [{ }, {}, ...},
+        # Dictionary structured as:
+        # msg_dict = { <subject1> : [{'key':<key>, 'text':<text>}, {...}, {...}],
+        #              <subject2>: [{...}, {...}],
         #               ... }
 
         if entry['done'] == False:  # Count the number of entries not marked as done
@@ -89,9 +42,7 @@ def test_2():
         #                                },
         #                                { 'subject':<subject2>, 'mails':[{}, {}, {}]}, ... ]
 
-    #number_of_msg = len(get_db_entries(done=False))
-
-    return render_template("test_2.html", list_of_entries=entry_list, number_of_msg=number_of_msg)
+    return render_template("index.html", list_of_entries=entry_list, number_of_msg=number_of_msg)
 
 
 # Define how to mark  an entry in db as 'done'.
@@ -104,7 +55,7 @@ def mark_as_done(key):  # Key to insure that it's the right entry
     update = {'done': True}  # Assign to 'done' field value=True
     update_entry(update, key)  # Update the entry in db
 
-    return redirect(f'{server_name}/test_2')  # Redirect to same page this action can be performed
+    return redirect(f'{server_name}/index')  # Redirect to same page this action can be performed
 
 
 @app.route('/done', methods=['GET', 'POST'])
@@ -139,3 +90,20 @@ def mark_undone(key):
     update_entry(update, key)   # Update entry in db
 
     return redirect(f'{server_name}/done')  # Redirect to same page this action can be performed
+
+
+@app.route('/test_2', methods=['GET', 'POST'])
+def test_2():
+
+    all_entries = get_db_entries()  # Get all entries from database
+    button = ButtonInput()
+
+    if button.validate_on_submit():  # Should fetch new messages and refresh db. Runs locally, Deta can't access email account
+
+        get_dict_of_msg()  # Fetch new messages from email account and recreate dictionary of messages from the updated db
+
+        # Print to the terminal
+        print('Updated!')
+        return redirect('test_2')
+
+    return render_template("test_2.html", all_entries=all_entries, form=button)
